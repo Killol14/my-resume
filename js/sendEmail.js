b5d523fcd94bc96c11d7aaadc2d76cd63222f02c
Killol14 <kgselectrical.plumbing@gmail.com>
2023-01-31 21:47:24 +0000

    Implement emailjs integration on contact page

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,1 +1,14 @@
-console.log("Hello!")
+function sendMail(contactForm) {
+    emailjs.send("service_zz2p4jo","K1G7S19",  {
+        "form_name": contactForm.name.value,
+        "form_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error)
+    });
+}
